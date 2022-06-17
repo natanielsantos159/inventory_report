f4a2f8c0185a0531a7323cee2efe5fbe9eb6b80f
Nataniel Santos <nathan.santos159@hotmail.com>
2022-06-17 19:37:06 -0300

    Requisito 1 concluido

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,27 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_info = {
+        "id": 1,
+        "nome_do_produto": "Caneca",
+        "nome_da_empresa": "Trybe",
+        "data_de_fabricacao": "12-01-2022",
+        "data_de_validade": "12-01-2099",
+        "numero_de_serie": 1234567,
+        "instrucoes_de_armazenamento": "instrucao de armazenamento",
+    }
+
+    caneca = Product(
+        product_info["id"],
+        product_info["nome_do_produto"],
+        product_info["nome_da_empresa"],
+        product_info["data_de_fabricacao"],
+        product_info["data_de_validade"],
+        product_info["numero_de_serie"],
+        product_info["instrucoes_de_armazenamento"],
+    )
+
+    for key, value in product_info.items():
+        assert key in caneca.__dict__.keys()
+        assert caneca.__dict__[key] == value
