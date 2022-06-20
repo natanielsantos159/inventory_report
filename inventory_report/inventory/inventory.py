2ea1ae425b06abc72d82f9cc45f6f055f63dcc08
Nataniel Santos <nathan.santos159@hotmail.com>
2022-06-20 15:41:19 -0300

    Requisito 7 refatorado

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,17 +5,17 @@ from inventory_report.reports.complete_report import CompleteReport
 import xmltodict
 
 
-class Inventory():
+class Inventory:
     def import_data(path, report_type):
         with open(path, encoding="utf-8") as file:
-            if path.endswith(".csv"):
-                file_reader = csv.DictReader(file, delimiter=",")
-                data = list(file_reader)
-            elif path.endswith(".json"):
-                data = json.load(file)
-            elif path.endswith(".xml"):
-                xml_string = file.read()
-                data = xmltodict.parse(xml_string)['dataset']['record']
+            file_extension = path.split('.')[-1]
+            read_by_extension = {
+                "csv": Inventory.read_csv,
+                "json": Inventory.read_json,
+                "xml": Inventory.read_xml
+            }
+
+            data = read_by_extension.get(file_extension)(file)
 
         if report_type == "simples":
             result_report = SimpleReport.generate(data)
@@ -23,3 +23,14 @@ class Inventory():
             result_report = CompleteReport.generate(data)
 
         return result_report
+
+    def read_csv(file):
+        file_reader = csv.DictReader(file, delimiter=",")
+        return list(file_reader)
+
+    def read_json(file):
+        return json.load(file)
+
+    def read_xml(file):
+        xml_string = file.read()
+        return xmltodict.parse(xml_string)['dataset']['record']
